a92923680c86aa8836aa89c89d7721b68a600012
JJ-TheDragon <61333289+JJ-TheDragon@users.noreply.github.com>
2023-03-25 15:28:20 -0500

    This is the code without app jar

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main():
     for m in get_monitors():
         height = int(m.height * .8)
         width = int(m.width * .8)
-<<<<<<< HEAD
     
     app.setSize(width, height)
 
@@ -53,25 +52,6 @@ def launch(win):
     app.showSubWindow(win)
     app.go(startWindow="one")
 
-=======
-
-    
-    root = tk.Tk()
-    root.title("Tab Widget")
-    tabControl = ttk.Notebook(root)
-    tab1 = ttk.Frame(tabControl)
-    tab2 = ttk.Frame(tabControl)
-    tabControl.add(tab1, text='Tab1')
-    tabControl.add(tab2, text='Tab 2')
-    tabControl.pack(expand=1, fill = 'both')
-    ttk.Label(tab1, text="Meternome").grid(column=0, row=0, padx=30, pady=30)
-    ttk.Label(tab2, text="File Organizer").grid(column=0, row=0, padx=30, pady=30)
-    objectMeternome = meternome.Meternome(120)
-    B = tk.Button(tab1, text ="Meternome", command = objectMeternome.meternomeClick())
-    root.mainloop()
-    
-
->>>>>>> 84432780632c58469667424ab57cdc9329063dcc
 
 if __name__ == "__main__":
     main()
