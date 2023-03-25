caac156bf63dabe655d5deb263c569ffa3a33074
Trevor Garnett <tjg0020@uah.edu>
2023-03-24 20:59:58 -0500

    created window with set size

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from appJar import gui
+from screeninfo import get_monitors
 
 def main():
+    app = main_screen()
+    app.go()
+    
+
+def main_screen():
+    height = 0
+    width = 0
+    for m in get_monitors():
+        height = int(m.height * .8)
+        width = int(m.width * .8)
     app = gui()
     app.addLabel("title", "Welcome to appJar")
+    app.setSize(width, height)
     app.setLabelBg("title", "red")
-    app.go()
+    return app
+
 
 
 if __name__ == "__main__":
