e3ffad42b73789bd32b01f149fa1d50133bca347
Trevor Garnett <tjg0020@uah.edu>
2023-03-24 22:14:15 -0500

    progress

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 from appJar import gui
 from screeninfo import get_monitors
+from colour import Color
 
 def main():
-    app = main_screen()
-    app.go()
-    
-
-def main_screen():
     height = 0
     width = 0
     for m in get_monitors():
         height = int(m.height * .8)
         width = int(m.width * .8)
     app = gui()
-    app.addLabel("title", "Welcome to appJar")
     app.setSize(width, height)
-    app.setLabelBg("title", "red")
-    return app
+
+    app.startTabbedFrame("TabbedFrame")
+    app.startTab("Metronome")
+    app.addCanvas("c1")
+    app.addCanvasOval("c1", int(height/2)+100, int(width/5), 200, 200, fill="black", outline="black", width=3)
+    app.addCanvasText("c1", 10, 10,"Help")
+    app.stopTab()
+
+    app.startTab("Pitch Generator")
+    app.addLabel("l2", "Tab 2 Label")
+    app.stopTab()
+
+    app.startTab("Tuner")
+    app.addLabel("l3", "Tab 3 Label")
+    app.stopTab()
+
+    app.startTab("File Save")
+    app.addLabel("l4", "Tab 4 Label")
+    app.stopTab()
+    app.stopTabbedFrame()
+    app.go()
 
 
 
